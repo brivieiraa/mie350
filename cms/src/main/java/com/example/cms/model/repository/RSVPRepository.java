f9e547e835f74a3caff92f3c4eb584f80768baee
kelvincklo <kelvinck.lo@mail.utoronto.ca>
2023-10-30 17:09:45 -0400

    Added typical code for some repositories and controllers

--- cms/src/main/java/com/example/cms/model/repository/RSVPRepository.java
+++ cms/src/main/java/com/example/cms/model/repository/RSVPRepository.java
@@ -1,5 +1,11 @@
 package com.example.cms.model.repository;
 
+import com.example.cms.model.entity.RSVP;
+import com.example.cms.model.entity.RSVPKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 // NEEDS COMPLETING //
-public interface RSVPRepository {
+@Repository
+public interface RSVPRepository extends JpaRepository<RSVP, RSVPKey> {
 }
--- cms/src/main/java/com/example/cms/model/repository/StudentGroupRepository.java
+++ cms/src/main/java/com/example/cms/model/repository/StudentGroupRepository.java
@@ -1,5 +1,10 @@
 package com.example.cms.model.repository;
 
+import com.example.cms.model.entity.StudentGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 // NEEDS COMPLETING //
-public interface StudentGroupRepository {
+@Repository
+public interface StudentGroupRepository extends JpaRepository<StudentGroup, String> {
 }
