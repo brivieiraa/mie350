09f72b1e124b833109d0bb691e602af764161045
shawn-teh <99860069+shawn-teh@users.noreply.github.com>
2023-11-21 02:22:26 -0500

    addRSVP test now works

--- cms/src/test/java/com/example/cms/RSVPTests.java
+++ cms/src/test/java/com/example/cms/RSVPTests.java
@@ -1,6 +1,7 @@
 package com.example.cms;
 
 import com.example.cms.model.entity.RSVP;
+import com.example.cms.model.entity.RSVPKey;
 import com.example.cms.model.entity.Student;
 import com.example.cms.model.repository.RSVPRepository;
 import com.example.cms.model.repository.StudentGroupRepository;
@@ -37,26 +38,23 @@ public class RSVPTests {
     void addRSVP() throws Exception{
 
         ObjectNode RSVPJson = objectMapper.createObjectNode();
+        RSVPJson.put("studentId", 1003L);
         RSVPJson.put("eventCode", 1);
-        RSVPJson.put("studentId", 2222L);
-
 
         MockHttpServletResponse response = mockMvc.perform(
-                        post("/RSVP").
+                        post("/rsvp").
                                 contentType("application/json").
                                 content(RSVPJson.toString()))
                 .andReturn().getResponse();
 
-        // assert HTTP code of response is 200 OK
         assertEquals(200, response.getStatus());
 
-        // Assert student with id 8888 exists in our repository and then get the student object
-        assertTrue(rsvpRepository.findById(1).isPresent());
-        RSVP addedRSVP = rsvpRepository.findById(2222).get();
+        assertTrue(rsvpRepository.findById(new RSVPKey(1003L,1)).isPresent());
+        RSVP addedRSVP = rsvpRepository.findById(new RSVPKey(1003L,1)).get();
 
         // Assert the details of the students are correct
-        assertEquals(1, addedRSVP.getRsvpKey());
-        assertEquals(2222L, addedRSVP.getStudent());
+        assertEquals(1003L, addedRSVP.getStudent().getId());
+        assertEquals(1, addedRSVP.getEvent().getEventCode());
         //assertEquals("MIE", addedStudent.getDepartment());
     }
 
